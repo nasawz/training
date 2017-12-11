5f7b53542ecd27d6fa4a7e6d873d1332b323d168
王喆 <nasa127@gmail.com>
2017-12-11 16:01:37 +0800

    request data

--- webpack4pwa/src/rss.tsx
+++ webpack4pwa/src/rss.tsx
@@ -1,55 +1,56 @@
 import * as React from 'react';
 import { List, Avatar, Icon } from 'antd';
-
-const listData = [];
-for (let i = 0; i < 5; i++) {
-  listData.push({
-    href: 'http://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
-const pagination = {
-  pageSize: 10,
-  current: 1,
-  total: listData.length,
-  onChange: (() => { }),
-};
-
-const IconText = ({ type, text }) => (
-  <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
-  </span>
-);
+import axios from 'axios'
+import _ from 'lodash'
 
 export interface RssProps {
 }
 
-export default class Rss extends React.Component<RssProps, any> {
+export interface RssState {
+  listData: Array<Object>
+}
+export default class Rss extends React.Component<RssProps, RssState> {
+  state: RssState
+
+  constructor(props: RssProps) {
+    super();
+    this.state = {
+      listData: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeed.cnblogs.com%2Fblog%2Fu%2F24081%2Frss').then((res) => {
+      let datas = []
+      _.map(res.data.items, (item) => {
+        datas.push({
+          href: item.link,
+          title: item.title,
+          description: item.description,
+          content: item.content,
+        })
+      })
+      this.setState({
+        listData: datas
+      });
+    }).catch((err) => {
+    })
+  }
+
   render() {
     return (
       <List
         itemLayout="vertical"
         size="large"
-        pagination={pagination}
-        dataSource={listData}
+        dataSource={this.state.listData}
         renderItem={item => (
-          <List.Item
-            key={item.title}
-            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
-            extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
-          >
+          <List.Item key={item.title}>
             <List.Item.Meta
-              avatar={<Avatar src={item.avatar} />}
               title={<a href={item.href}>{item.title}</a>}
               description={item.description}
             />
-            {item.content}
-          </List.Item>
+            <div dangerouslySetInnerHTML={{ __html: item.content }} />
+          </ List.Item>
         )}
       />
     );
